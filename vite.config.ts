f25f50a720571e1a9c028b5b2910224ec46eb456
feildmaster <feildmaster@users.noreply.github.com>
2024-07-07 17:49:57 -0500

    properly chunk out modules

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,35 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: [
+            'react',
+            'react-dom',
+            'react-router-dom',
+          ],
+          mui: [
+            '@mui/material',
+            '@mui/icons-material',
+            '@emotion/styled',
+            '@emotion/react',
+          ],
+          firebase: [
+            'firebase/app',
+            'firebase/auth',
+            'firebase/storage',
+            'firebase/firestore',
+          ],
+          plugins: [
+            'banana-i18n',
+            'axios',
+          ],
+        },
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
